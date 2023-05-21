a0f0d194c7001926dc6e6e20e78ed3b5fa88fb56
Vernon Grant <vernon@ruppell.io>
2023-05-21 19:52:24 +0200

    Adds extension to browser module files

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { initializeIndicator } from "./src/indicator";
-import { initializePanel } from "./src/panel";
-import { initializeTiles } from "./src/tiles";
+import { initializeIndicator } from "./src/indicator.js";
+import { initializePanel } from "./src/panel.js";
+import { initializeTiles } from "./src/tiles.js";
 
 initializeTiles("#tiles");
 initializePanel("#panel");
